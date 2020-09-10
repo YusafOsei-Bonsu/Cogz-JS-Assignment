b7633259856dc8027346d5ff9cbd0ee39121767b
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-10 19:55:49 +0100

    The sorting procedure

--- scripts/objectSort.js
+++ scripts/objectSort.js
@@ -1,3 +1,10 @@
 module.exports = objectSort = (objList, prop) => {
-
+    objList.sort((item, nextItem) => {
+        if (item[prop] > nextItem[prop]) {
+            return 1; // Current item should go before the next item
+        } else {
+            return -1; // Next item should precede the current item
+        }
+    });
+    return objList;
 }
