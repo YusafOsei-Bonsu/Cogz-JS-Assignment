b85a0dbf0e817588304877648bcef869096908b6
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-10 10:35:12 +0100

    Checks if the arguments are alphabetical

--- count.js
+++ count.js
@@ -1,10 +1,22 @@
 // Counts the amount of times a letter appears in the word
 module.exports = countLetters = (word, letter) => {
-    let sum = 0;
-    word.split('').forEach(l => {
-        if (l == letter) {
-            sum += 1;
-        }
-    });
-    return sum;
+
+    // Check if the fn arguments are alphabetical only
+    if ((typeof(word) == "string") && (typeof(letter) == "string")) {
+        
+        // for case-insensitive string comparison 
+        word = letter == letter.toLowerCase() ? word.toLowerCase() : word.toUpperCase();
+
+        let sum = 0;
+        word.split('').forEach(l => {
+            if (l == letter) sum += 1;
+        });
+        
+        return sum;
+    } else {
+        return "Enter alphabets only.";
+    }
+
 }
+
+console.log(typeof('2'));
