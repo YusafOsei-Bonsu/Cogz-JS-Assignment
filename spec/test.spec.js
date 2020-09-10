361a4aa897049dabbb0564dcdc06eb663fb08b69
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-10 11:56:51 +0100

    Test suite for reverse fn

--- spec/test.spec.js
+++ spec/test.spec.js
@@ -1,12 +1,13 @@
 const fileSystem = require('fs');
-const countLetters = require('../count.js');
+const countLetters = require('../scripts/count.js');
+const reverse = require('../scripts/reverse.js');
 
 describe('JS Assignments', () => {
     
     // Test suite for the letter-counting JS fn 
-    describe('Letter-counting JS Function', () => {
+    describe('Letter-counting JS Fn', () => {
         // Check if the file exists
-        it('File should exist', () => expect(fileSystem.statSync('count.js')).toBeTruthy());
+        it('File should exist', () => expect(fileSystem.statSync('./scripts/count.js')).toBeTruthy());
         
         // Check how many times 'a' occurs in the word "radar"
         it("Should return 2 when counting a's in 'radar'", () => expect(countLetters("radar", "a")).toBe(2));
@@ -34,5 +35,20 @@ describe('JS Assignments', () => {
 
         // Checks if an error message returns if both arguments are non-alphabetical strings
         it("Should return an error message if both of the arguments are non-alphabetical Strings", () => expect(countLetters('1', '2')).toBe("Enter alphabets only."));
+    });
+    
+    // Test suite for reverse-number JS fn
+    describe('Reverse-number JS Fn', () => {
+        // Check if the file exists
+        it('File should exist', () => expect(fileSystem.statSync('./scripts/reverse.js')).toBeTruthy());
+        
+        // Different lengths of numbers 
+        it('Should return 1', () => expect(reverse(1)).toBe(1));    
+        it('Should return 005', () => expect(reverse(500)).toBe(005));
+        it('Should return 0000000001', () => expect(reverse(1000000000)).toBe(0000000001));
+
+        // Checks if the fn returns an error message in response to recieving non-integer input 
+        it('Should return an error message in response to alphabetical arguments', () => expect(reverse("hello world")).toBe("Enter whole numbers only."));
+        it('Should return an error message in response to a decimal argument', () => expect(reverse(1.5)).toBe("Enter whole numbers only."));
     });
 })
