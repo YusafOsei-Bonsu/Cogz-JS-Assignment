5db9cd3015554639119ae10a177e977116c64f89
Yusaf Osei-Bonsu <46824238+YusafOsei-Bonsu@users.noreply.github.com>
2020-09-11 21:17:48 +0100

    initial

--- scripts/getKeys.js
+++
@@ -1,2 +0,0 @@
-// Returns an object's own and inherited properties
-module.exports = getKeys = (obj) => (typeof(obj) != "object") ? [] : Object.keys(obj);
--- scripts/objectLength.js
+++
@@ -1,7 +0,0 @@
-module.exports = objectLength = (obj) => {
-    let size = 0;
-    for (let key in obj) {
-        if (obj.hasOwnProperty(key)) size += 1;
-    }
-    return size;
-}
